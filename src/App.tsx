42c3ce7f4e815f325cd1a87deb2d50496790983b
Suminda Gunawardana <suminda.d.g@gmail.com>
2023-12-11 19:38:38 +1100

    added today

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent } from 'react';
 import { useTimerContext } from './contexts/useTimerContext'
 import { useGlobalContext } from './contexts/useGlobalContext';
 import SettingsModal from './components/SettingsModal';
+import { formatDate } from './util/util';
 
 function App() {
   const { changeEndTimeFromString, isExpired, days, hours, minutes, seconds } = useTimerContext()
@@ -28,7 +29,7 @@ function App() {
       }
       <div
         id="bgMain"
-        className='flex justify-center h-full w-full items-center'>
+        className='flex gap-16 flex-col justify-center h-full w-full items-center'>
         <div onClick={handleClickTimer} id="timerMain" className='relative transition-all p-24 shadow-2xl hover:shadow-3xl hover:scale-[100.5%] rounded-full bg-white flex justify-center items-center'>
           {
             !isExpired ? (
@@ -63,6 +64,11 @@ function App() {
           }
 
         </div>
+        <div onClick={handleClickTimer} id="nowDisplay" className='relative transition-all p-24 shadow-2xl hover:shadow-3xl hover:scale-[100.5%] rounded-full bg-gray-200 flex justify-center items-center'>
+          <h2>{formatDate(new Date().toString())}</h2>
+
+        </div>
+
         <div className='absolute transition-all bottom-0 right-0 flex items-center justify-center z-20 opacity-0 hover:opacity-100 p-4'>
           <input className='px-4 py-2 rounded-full shadow-2xl' onChange={handleEndTimeChange} type="date" name="" id="" />
         </div>
--- src/util/util.ts
+++ src/util/util.ts
@@ -1,29 +1,65 @@
 import { CONSTANTS } from "./constants";
 
 export interface IMonthData {
-    text: string;
-    shortText: string;
-    lastDate: number;
-  }
+  text: string;
+  shortText: string;
+  lastDate: number;
+}
 
 
 export function getMonthData(year: number, monthString: string): IMonthData | null {
-    const monthIndex = CONSTANTS.months.findIndex(m => m.toLowerCase() === monthString.toLowerCase())
-    if (typeof monthIndex === 'undefined') {
-        return null
-    }
+  const monthIndex = CONSTANTS.months.findIndex(m => m.toLowerCase() === monthString.toLowerCase())
+  if (typeof monthIndex === 'undefined') {
+    return null
+  }
 
-    const monthIndex1 = monthIndex + 1;
-    const month = CONSTANTS.months[monthIndex]
-  
-    // Get the last date of the current month and year
-    const lastDate = new Date(year, monthIndex1, 0).getDate();
-  
-    return {
-      text: month,
-      shortText: month.substring(0, 3),
-      lastDate
-    };
+  const monthIndex1 = monthIndex + 1;
+  const month = CONSTANTS.months[monthIndex]
+
+  // Get the last date of the current month and year
+  const lastDate = new Date(year, monthIndex1, 0).getDate();
+
+  return {
+    text: month,
+    shortText: month.substring(0, 3),
+    lastDate
+  };
 }
 
-  
+
+export function formatDate(dateString: string): string {
+  const months = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+
+  const daysOfWeek = [
+    'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
+  ];
+
+  const date = new Date(dateString);
+
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  const dayOfWeek = daysOfWeek[date.getDay()];
+  const dayOfMonth = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  const ordinalSuffix = (dayOfMonth) => {
+    if (dayOfMonth >= 11 && dayOfMonth <= 13) {
+      return 'th';
+    }
+    switch (dayOfMonth % 10) {
+      case 1: return 'st';
+      case 2: return 'nd';
+      case 3: return 'rd';
+      default: return 'th';
+    }
+  };
+
+  const formattedDate = `${hours}:${minutes}:${seconds} ${dayOfWeek} ${dayOfMonth}${ordinalSuffix(dayOfMonth)} ${month}, ${year}`;
+  return formattedDate;
+}
+
